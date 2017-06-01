54a0c130250e3362967154bd481726a3917cc3d6
Alexander Diaz <john2005_16@yahoo.com>
2017-06-01 11:42:00 -0500

    Change

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-'use strict' // Nuevas funcionalidades de Ecmascript 6, nuevos tipos de  variables, Buenas prácticas
+'use strict'
 
 const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
 mongoose.connect(config.db, (err, res) => {
-	if (err) {
-		return console.log(`Error al conectar a la base de datos: ${err}`)
-	}
-	console.log('conexion a la base de datos establecida...')
-	app.listen(config.port, () => {
-		console.log(`API REST corriendo en http://localhost:${config.port}`) // El caracter ` lo encontre AltGr + "la tecla ñ+2 a la derecha"
-	})
+  if (err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conexión a la base de datos establecida...')
 
+  app.listen(config.port, () => {
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+  })
 })
